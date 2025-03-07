78d749ae1bc31a8df796e8f5f011869bc3e9ebcb
GalaCad2003 <64437004+GalaCad2003@users.noreply.github.com>
2025-03-06 22:45:48 -0500

    Add files via upload

---
+++ Joker.lua
@@ -0,0 +1,135 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Phantom Thief
+--- MOD_ID: 1234
+--- MOD_AUTHOR: [GalaCad]
+--- MOD_DESCRIPTION: Adds Joker from Persona 5 as a Joker
+--- BADGE_COLOUR: #ff0000
+--- DISPLAY_NAME: P5 Joker
+--- PRIORITY: -100
+---MOD CODE---
+SMODS.Atlas{
+  key = "P5JokerMod",
+  path = "P5CARD2.png",
+  px = 71,
+  py = 95,
+}
+
+	SMODS.Joker {
+  key = 'P5JokerMod_joker',
+  loc_txt = {
+  name = 'Phantom Thief',
+    text = {"Destroys played {C:hearts}Heart{} cards and adds {X:mult,C:white} X#2# {} Mult {C:inacative}(Currently {X:mult,C:white} X#1# {C:inactive} Mult)"}
+  },
+  config = { extra = { Xmult = 1, Xmult_mod  = 1} },
+  loc_vars = function(self, info_queue, card)
+    return { vars = { card.ability.extra.Xmult, card.ability.extra.Xmult_mod } }
+  end,
+  rarity = 3,
+  atlas = 'P5JokerMod',
+  pos = { x = 0, y = 0 },
+  cost = 5,
+  calculate = function(self,card,context)
+    ---context.after is after the hand is scored
+    ---not context.blueprint means this part cannot be copied by Blueprint, context.destroying_card means it can destroy cards without making ghost cards
+    if not context.blueprint and context.destroying_card and context.destroying_card:is_suit('Hearts') then
+
+      card.ability.extra.Xmult = card.ability.extra.Xmult + card.ability.extra.Xmult_mod
+      return true
+    end
+      ---return the xmult if it's above 1, this removes unnecessary animations/calculations if the xmult is 1, because no change would be made anyway
+      if context.joker_main and card.ability.extra.Xmult >= 1 then
+        return {
+        ---text pops up below the joker to show it's giving xmult
+        message = localize{type='variable',key='a_xmult',vars={card.ability.extra.Xmult}},
+        Xmult_mod = card.ability.extra.Xmult,
+        colour = G.C.MULT,
+        ---card = card tells the game the message refers to this joker
+        card = card
+        }
+      end
+    end
+}
+SMODS.Challenge{
+  key = 'P5JokerMod_challenge',
+  loc_txt = {
+          name = 'Take Your Heart',
+            },
+  jokers = {
+           {id = 'j_1234_P5JokerMod_joker', eternal = true}
+  },
+   restrictions = {
+              banned_cards = {},
+               banned_other = {},
+   },
+   
+deck = {
+  cards = {
+    {s='H', r='A'},
+    {s='H', r='2'},
+    {s='H', r='3'},
+    {s='H', r='4'},
+    {s='H', r='5'},
+    {s='H', r='6'},
+    {s='H', r='7'},
+    {s='H', r='8'},
+    {s='H', r='9'},
+    {s='H', r='T'},
+    {s='H', r='J'},
+    {s='H', r='Q'},
+    {s='H', r='K'},
+    {s='H', r='A'},
+    {s='H', r='2'},
+    {s='H', r='3'},
+    {s='H', r='4'},
+    {s='H', r='5'},
+    {s='H', r='6'},
+    {s='H', r='7'},
+    {s='H', r='8'},
+    {s='H', r='9'},
+    {s='H', r='T'},
+    {s='H', r='J'},
+    {s='H', r='Q'},
+    {s='H', r='K'},
+    {s='H', r='A'},
+    {s='H', r='2'},
+    {s='H', r='3'},
+    {s='H', r='4'},
+    {s='H', r='5'},
+    {s='H', r='6'},
+    {s='H', r='7'},
+    {s='H', r='8'},
+    {s='H', r='9'},
+    {s='H', r='T'},
+    {s='H', r='J'},
+    {s='H', r='Q'},
+    {s='H', r='K'},
+    {s='H', r='A'},
+    {s='H', r='2'},
+    {s='H', r='3'},
+    {s='H', r='4'},
+    {s='H', r='5'},
+    {s='H', r='6'},
+    {s='H', r='7'},
+    {s='H', r='8'},
+    {s='H', r='9'},
+    {s='H', r='T'},
+    {s='H', r='J'},
+    {s='H', r='Q'},
+    {s='H', r='K'},
+    {s='H', r='A'},
+    {s='H', r='2'},
+    {s='H', r='3'},
+    {s='H', r='4'},
+    {s='H', r='5'},
+    {s='H', r='6'},
+    {s='H', r='7'},
+    {s='H', r='8'},
+    {s='H', r='9'},
+    {s='H', r='T'},
+    {s='H', r='J'},
+    {s='H', r='Q'},
+    {s='H', r='K'}
+        --yes_suits = { H = true }--
+           }
+        }
+      }
